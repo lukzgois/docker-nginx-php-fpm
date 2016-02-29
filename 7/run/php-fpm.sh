fadd464ab18bc5e9efb700a41f304c914c1ac347
Lukz <lucaspgois@gmail.com>
2016-02-29 18:02:21 -0300

    Changes on dockerfile and code style
    - Replaced tabs with spaces
    - Make some modifications to the php-fpm pool
    - Tweak the nginx to run with the correct number of workers

--- 7/run/php-fpm.sh
+++ 7/run/php-fpm.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [ ! -z "$APP_DIR" ]; then
-	DOCUMENT_ROOT="/var/www"
-	rm -rf ${DOCUMENT_ROOT}
+    DOCUMENT_ROOT="/var/www"
+    rm -rf ${DOCUMENT_ROOT}
 
-	if [ -d "${APP_DIR}/public" ]; then
-		dir="${DOCUMENT_ROOT}"
-	else
-		dir="${DOCUMENT_ROOT}/public"
-	fi
+    if [ -d "${APP_DIR}/public" ]; then
+        dir="${DOCUMENT_ROOT}"
+    else
+        dir="${DOCUMENT_ROOT}/public"
+    fi
 
-	mkdir -p `dirname ${dir}`
-	ln -s $APP_DIR $dir
+    mkdir -p `dirname ${dir}`
+    ln -s $APP_DIR $dir
 fi
 
 exec /usr/sbin/php-fpm7.0 --nodaemonize --allow-to-run-as-root >> /var/log/php7.0-fpm.log 2>&1
