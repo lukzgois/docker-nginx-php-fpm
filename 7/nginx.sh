8d7ff0fa6f13723b98d853121cb217080e959046
Lukz <lucaspgois@gmail.com>
2016-03-08 21:06:15 -0300

    Fixing nginx config bug

--- 7/nginx.sh
+++ 7/nginx.sh
@@ -2,7 +2,7 @@
 
 # Installing NGINX
 apt-get install -y --force-yes nginx
-echo "\ndaemon off;" >> /etc/nginx/nginx.conf
+echo "daemon off;" >> /etc/nginx/nginx.conf
 sed -i -e "s/user www-data/user root/g" /etc/nginx/nginx.conf
 rm /etc/nginx/sites-available/default
 cp /build/templates/virtualhost /etc/nginx/sites-available/default
