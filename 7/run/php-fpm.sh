97f64ca613660b8efd9924863c1d804c9eb61b09
Lukz <lucaspgois@gmail.com>
2016-03-29 13:33:29 -0300

    fix sed command in php7.0 service

--- 7/run/php-fpm.sh
+++ 7/run/php-fpm.sh
@@ -15,16 +15,16 @@ if [ ! -z "$APP_DIR" ]; then
 fi
 
 # Setting  some PHP config for development
-RUN sed -i "s/error_reporting = .*/error_reporting = $PHP_ERROR_REPORTING/" /etc/php/7.0/cli/php.ini
-RUN sed -i "s/display_errors = .*/display_errors = $PHP_DISPLAY_ERRORS/" /etc/php/7.0/cli/php.ini
-RUN sed -i "s/memory_limit = .*/memory_limit = $PHP_MEMORY_LIMIT/" /etc/php/7.0/cli/php.ini
-RUN sed -i "s/;date.timezone.*/date.timezone = $PHP_TIMEZONE/" /etc/php/7.0/cli/php.ini
+sed -i "s/error_reporting = .*/error_reporting = $PHP_ERROR_REPORTING/" /etc/php/7.0/cli/php.ini
+sed -i "s/display_errors = .*/display_errors = $PHP_DISPLAY_ERRORS/" /etc/php/7.0/cli/php.ini
+sed -i "s/memory_limit = .*/memory_limit = $PHP_MEMORY_LIMIT/" /etc/php/7.0/cli/php.ini
+sed -i "s/;date.timezone.*/date.timezone = $PHP_TIMEZONE/" /etc/php/7.0/cli/php.ini
 
-RUN sed -i "s/error_reporting = .*/error_reporting = $PHP_ERROR_REPORTING/" /etc/php/7.0/fpm/php.ini
-RUN sed -i "s/display_errors = .*/display_errors = $PHP_DISPLAY_ERRORS/" /etc/php/7.0/fpm/php.ini
-RUN sed -i "s/memory_limit = .*/memory_limit = $PHP_MEMORY_LIMIT/" /etc/php/7.0/fpm/php.ini
-RUN sed -i "s/upload_max_filesize = .*/upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE/" /etc/php/7.0/fpm/php.ini
-RUN sed -i "s/post_max_size = .*/post_max_size = $PHP_POST_MAX_SIZE/" /etc/php/7.0/fpm/php.ini
-RUN sed -i "s/;date.timezone.*/date.timezone = $PHP_TIMEZONE/" /etc/php/7.0/fpm/php.ini
+sed -i "s/error_reporting = .*/error_reporting = $PHP_ERROR_REPORTING/" /etc/php/7.0/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = $PHP_DISPLAY_ERRORS/" /etc/php/7.0/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = $PHP_MEMORY_LIMIT/" /etc/php/7.0/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE/" /etc/php/7.0/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = $PHP_POST_MAX_SIZE/" /etc/php/7.0/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = $PHP_TIMEZONE/" /etc/php/7.0/fpm/php.ini
 
 exec /usr/sbin/php-fpm7.0 --nodaemonize --allow-to-run-as-root >> /var/log/php7.0-fpm.log 2>&1
